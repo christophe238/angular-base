aded2faa3493bfadc5f6b1b25cc2a541fc5f015e
Christophe Marchadour <christophe.marchadour@gmail.com>
2015-03-02 12:18:26 +0100

    Fix jade task issue for windows environment

--- grunt-config/tasks/jade.js
+++ grunt-config/tasks/jade.js
@@ -6,8 +6,10 @@ module.exports = function(grunt) {
 
     var buildDir = require('../buildDir.js')();
 
-    var mainPathInterfix = path.join('src', 'modules') + path.sep;    
-    var jadePathInterfix = path.join('jade','views') + path.sep;
+
+    //For windows build replace \ by /
+    var mainPathInterfix = (path.join('src', 'modules') + path.sep).replace(/\\/g,'/');
+    var jadePathInterfix = (path.join('jade','views') + path.sep).replace(/\\/g,'/');
 
     var dest = path.join(buildDir, 'tmp', 'views');
     var rootDest = path.join(buildDir);
@@ -15,9 +17,9 @@ module.exports = function(grunt) {
         path.join('src', 'modules', '**', '*.jade')
         ], dest, {
             rename: function(base, destPath) {
-                var res = path.join(base, destPath.replace(/\.jade$/, '.js'));
-                res = res.replace(mainPathInterfix, '');
-                res = res.replace(jadePathInterfix, '');
+                var res = path.join(base, destPath.replace(/\.jade$/, '.js')).replace(/\\/g,'/');
+                res = res.replace(mainPathInterfix, '').replace(/\\/g,'/');
+                res = res.replace(jadePathInterfix, '').replace(/\\/g,'/');
                 return res;
             }
         });
@@ -27,30 +29,32 @@ module.exports = function(grunt) {
         dest: path.join(buildDir,'index.html')
     });
     grunt.config('jade', {
-        devall: {        
+        devall: {
             options: {
                 amd: true,
                 client: true,
                 compileDebug: false,
                 namespace: 'TEMPLATES',
-                processName: function(filename){  
+                processName: function(filename){
                     var res = filename.replace(/\.jade$/, '');
+                    res = res.replace(/\\/g,'/');
                     res = res.replace(mainPathInterfix, '');
-                    res = path.join('views',res.replace(jadePathInterfix, '')); 
+                    res = path.join('views',res.replace(jadePathInterfix, '')).replace(/\\/g,'/');
+
                     console.log("jade : " + res + " compiled");
                     return res;
                 }
             },
-            files: files   
+            files: files
         },
-        devindex: {        
+        devindex: {
             options: {
                 client: false,
                 processName: function(filename){
                     console.log("jade : " + filename + " compiled");
                 }
             },
-            files: indexFile   
+            files: indexFile
         }
     });
 };
